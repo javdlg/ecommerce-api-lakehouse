18bf5f673a04ccfdee42e69f3731805cb7ff46af
javdlg <javier.dlogokiski@gmail.com>
2026-03-30 14:43:42 -0300

    feat: Refactor fetch_items script to retrieve unique product IDs from MercadoLibre API and improve logging

--- src/extract/fetch_items.py
+++ src/extract/fetch_items.py
@@ -1,5 +1,7 @@
+import time
 import json
 import logging
+import requests
 from src.api_client.meli_client import MeliClient
 
 logging.basicConfig(
@@ -7,19 +9,63 @@ logging.basicConfig(
 )
 
 
+def get_product_ids_from_search(category="MLA1055", limit=50):
+    """
+    The scout: This function uses the public API (without token) to evade the firrewall and collect the IDs from the products catalog
+    """
+    url = f"https://api.mercadolibre.com/sites/MLA/search?category={category}&limit={limit}"
+
+    # We use a browser user-agent, but without the authentication: Bearer
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+    }
+
+    logging.info(f"Tracking products in category {category}")
+    response = requests.get(url, headers=headers)
+
+    if response.status_code != 200:
+        logging.error(
+            f"Failed to track products. HTTP {response.status_code}: {response.text}"
+        )
+        return []
+
+    data = response.json()
+    results = data.get("results", [])
+
+    product_ids = []
+    for items in results:
+        # We search for the specific catalog ID (Product), no the item ID (Publication)
+        cat_id = items.get("catalog_product_id")
+
+        # We filter to ensure that exists and is not duplicated
+        if cat_id and cat_id not in product_ids:
+            product_ids.append(cat_id)
+
+    logging.info(
+        f"Found {len(product_ids)} unique and valid product IDs in category {category}"
+    )
+    return product_ids
+
+
 def main():
-    print("Initializing direct extraction by Item ID...")
+    print("Initializing batch extraction - Bronze layer...")
     client = MeliClient()
 
-    # List of real cell phone IDs on Mercado Libre Argentina (MLA)
-    # You can replace these with any valid item IDs from the MLA marketplace
-    item_ids = [
-        "MLA63468990",
-        "MLA2071412843",
-        "MLA2060138354",
-    ]
+    # 1. The scout goes to find the product IDs in the category (first with 50)
+    product_ids = get_product_ids_from_search(limit=50)
 
+    if not product_ids:
+        logging.error("No product IDs found. Exiting.")
+        return
+
+    # 2. We initialize our authenticated client to fetch the item details
+    client = MeliClient()
     collected_data = []
+
+    # 3. The worker processes the list
+    print(f"Extracting details for {len(product_ids)} products...")
+
+    # MODIFY ALL THE CODE BELOW LATER #
 
     for item_id in item_ids:
         data = client.get_item(item_id)
